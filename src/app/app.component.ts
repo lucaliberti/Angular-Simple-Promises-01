47b83fe1d3fa9b47c2a214d5e5c9e359eb76c1f8
lucaliberti <liberti.luca@gmail.com>
2022-02-15 21:23:04 +0100

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular-Simple-Promises';
+export class AppComponent implements OnInit { 
+ 
+  esito: string = "OK"
+
+  ngOnInit(): void {}
+
+  onCreaPromise():Promise<string> {
+    return new Promise(
+      (resolve, reject) => { this.innerFunction(resolve, reject) })
+
+    //this.p.then((msg) => { console.log(msg) });
+  }
+
+  innerFunction(resolve: Function, reject:Function) {
+    if (this.esito == "OK")
+      resolve("esito OK")
+    else
+      reject("esito KO")
+  }
+
+  checkPromise(){
+   let p:Promise<string>=this.onCreaPromise()
+   p.then((msg) => { console.log("then: ho ricevuto "+msg) })
+    .catch((error) => { console.log("catch: ho ricevuto "+error) })
+  }
+  
+ 
+
+  onImpostaEsitoOK() { 
+    this.esito = "OK" 
+    console.log("Impostato esito=", this.esito)
+  }
+  onImpostaEsitoKO() { 
+    this.esito = "KO"
+    console.log("Impostato esito=", this.esito) 
+  }
+
 }
